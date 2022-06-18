caacba3aa400dea97c09fd16ee4087a752604d95
Tushar <69718699+tushar2k19@users.noreply.github.com>
2022-06-19 02:25:49 +0530

    FInal Project
    Final Project

--- project/main.js
+++ project/main.js
@@ -4,10 +4,12 @@ let form = document.querySelector("#addForm");
 let lists = document.querySelector("#items");
 let search = document.querySelector("#filter");
 
+
 form.addEventListener("submit", additem);
 lists.addEventListener("click",remitem);
 
 search.addEventListener("keyup", filterItems);
+
 
 function additem(e){
     e.preventDefault();  
@@ -15,44 +17,79 @@ function additem(e){
     
     let li = document.createElement("li");
     li.className = "list-group-item";
-    li.innerHTML = item.value;
+    li.innerHTML = '<span class = "texts">' +item.value+'</span>';
     lists.appendChild(li); 
+    input.value = "";
     
     let deletebtn = document.createElement("button");
     deletebtn.className = "btn btn-danger btn-sm float-right delete";
-     deletebtn.appendChild(document.createTextNode("X"));
+    deletebtn.appendChild(document.createTextNode("X"));
     // can also be done as 
     // deletebtn.innerHTML = "X";
-    console.log(e.target);
     li.appendChild(deletebtn);
 
+    let editbtn = document.createElement("button");
+    editbtn.className = "btn btn-sm float-right Edit";
+    editbtn.appendChild(document.createTextNode("Edit"));
+    li.appendChild(editbtn);
+
+}
+// IMPORTANT
+function remitem(e)
+{
+        if(e.target.classList.contains("delete")) //there's a class named delete in the items 
+        {  
+            let confirmation = confirm("Are You Sure?");
+            if(confirmation){
+                let li = e.target.parentNode;
+                lists.removeChild(li);
+            }
+        }else
+
+        if(e.target.classList.contains("Edit"))
+        {
+            
+            let input = document.createElement("input");
+            let text = e.target.parentNode.querySelector(".texts").textContent;
+            input.value = text;
+            e.target.parentNode.removeChild(e.target.parentNode.firstElementChild);
+            let firstChild = e.target.parentNode.firstElementChild;
+            e.target.parentNode.insertBefore(input,firstChild);
+            e.target.innerHTML = "&#10003";
+            e.target.className = "btn btn-sm float-right Editing"
+        } else
+
+        if(e.target.classList.contains("Editing"))
+        {
+            let input = e.target.parentNode.querySelector("input");
+            let newtext = input.value;
+            let linewtext = document.createElement("span");
+            console.log(newtext);
+            linewtext.className = "texts";
+            linewtext.innerHTML = newtext;
+            e.target.parentNode.removeChild(e.target.parentNode.firstElementChild);
+            let firstChild = e.target.parentNode.firstElementChild;
+            e.target.parentNode.insertBefore(linewtext,firstChild);
+            e.target.innerHTML = "Edit";
+            
+            e.target.className = "btn btn-sm float-right Edit";
+            
+        }    
 }
 
-function remitem(e){
-    
-  /*imp*/  if(e.target.classList.contains("delete")){ //there's a class named delete in the 
-        let confirmation = confirm("Are You Sure?");
-        if(confirmation){
-            let li = e.target.parentNode;
-            lists.removeChild(li);
-        }
-    }
-    
-}
 
 function filterItems(e){
+   
     let text = e.target.value.toLowerCase();
     let allItems = lists.getElementsByTagName("li");
-    
+    //console.log(allItems);
     let arrayofitems = Array.from(allItems);
-
+    
     arrayofitems.forEach(function(item)
     {
-        let name = item.firstChild.textContent.toLowerCase();
-        // console.log(name);
+        let name = item.querySelector(".texts").textContent.toLowerCase();
         if(name.includes(text)==true)
         {
-            console.log(33);
             item.style.display = "block";
         }
         else{
@@ -60,3 +97,4 @@ function filterItems(e){
         }
     });
 }
+
